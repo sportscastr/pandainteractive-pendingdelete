aea063637d2739d6fbc31f32918e2095b143957b
amccalla <amccalla1912@gmail.com>
2021-02-10 12:16:34 -0500

    initial setup

---
+++ pandainteractive/pandainteractive.h
@@ -0,0 +1,18 @@
+//
+//  pandainteractive.h
+//  pandainteractive
+//
+//  Created by Drew McCalla on 2/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pandainteractive.
+FOUNDATION_EXPORT double pandainteractiveVersionNumber;
+
+//! Project version string for pandainteractive.
+FOUNDATION_EXPORT const unsigned char pandainteractiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pandainteractive/PublicHeader.h>
+
+
